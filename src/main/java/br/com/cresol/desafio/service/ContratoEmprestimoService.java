86a9634f0ea510c9b50f257a1ea1a9c7682e1f81
Cleiton Janke <cleiton.janke@gmail.com>
2019-12-29 19:08:53 -0300

    Inclusão de documentação ('javadoc') nas classes de serviços internos da 'Simulação de Empréstimos', apenas para melhor entendimento das implementações.

--- src/main/java/br/com/cresol/desafio/service/ContratoEmprestimoService.java
+++ src/main/java/br/com/cresol/desafio/service/ContratoEmprestimoService.java
@@ -12,6 +12,12 @@ import br.com.cresol.desafio.manager.ContratoEmprestimoManager;
 import br.com.cresol.desafio.util.CalculoUtil;
 import br.com.cresol.desafio.util.ValidacaoUtil;
 
+/**
+ * Serviço interno responsável por obter e registrar os dados dos contratos
+ * envolvidos nas requisições de Simulação de Empréstimos.
+ * 
+ * @author Cleiton Janke
+ */
 public class ContratoEmprestimoService {
 
 	private static final BigDecimal VALOR_LIMITE_MENOR_TAXA_JUROS = new BigDecimal(1000);
@@ -32,6 +38,26 @@ public class ContratoEmprestimoService {
 		this.contratoManager = contratoManager;
 	}
 
+	/**
+	 * Obtém os dados de um contrato de empréstimo a partir dos parãmetros
+	 * informados na requisição.<br>
+	 * ► Se os parâmetros forem os mesmo de algum contrato já registrado e ainda
+	 * válido (não expirado), então apenas retorna os dados, sem efetuar o
+	 * recálculo.<br>
+	 * ► Se os parâmetros forem os mesmo, mas o contrato registrado já esteja
+	 * expirado, então exclui o contrato registrado e recalcula os valores.<br>
+	 * ► Se não houver contrato previamente registrado, efetua os cálculos e
+	 * registra os dados.<br>
+	 * 
+	 * @param cpfPessoa          {@link String} : CPF da pessoa envolvida na
+	 *                           requisição do empréstimo.
+	 * @param valorContrato      {@link BigDecimal} : valor total do contrato de
+	 *                           empréstimo.
+	 * @param quantidadeParcelas {@link Integer} : quantidade de parcelas da
+	 *                           requisição de empréstimo.
+	 * @return {@link ContratoEmprestimo} : objeto com os dados do contrato.
+	 * @throws Exception : exceção ao validar os parâmetros ou obter os dados.
+	 */
 	public ContratoEmprestimo getDadosContrato(final String cpfPessoa, final BigDecimal valorContrato,
 			final Integer quantidadeParcelas) throws Exception {
 		ValidacaoUtil.validaParametrosParaContrato(cpfPessoa, valorContrato, quantidadeParcelas);
@@ -61,6 +87,23 @@ public class ContratoEmprestimoService {
 		return contrato;
 	}
 
+	/**
+	 * Efetua o cálculo da taxa de juros a ser usada para calcular o valor das
+	 * parcelas, tomando por base as seguintes regras:<br>
+	 * ► Se o VALOR_CONTRATO for menor ou igual do que
+	 * {@link #VALOR_LIMITE_MENOR_TAXA_JUROS}, então a menor taxa de juros, de
+	 * "1,8%" é aplicada.<br>
+	 * ► Caso contrário a taxa de juros será de "3%".<br>
+	 * ► Caso a quantidade de parcelas ultrapasse o
+	 * {@link #QUANTIDADE_PARCELAS_SEM_TAXA_ADICIONAL}, então será adicionado uma
+	 * taxa de "0,5%" à taxa de juros já definida.<br>
+	 * 
+	 * @param valorContrato      {@link BigDecimal} : valor do emprestimo, informado
+	 *                           na requisição.
+	 * @param quantidadeParcelas {@link Integer} : quantidade de parcelas desejadas,
+	 *                           informada na requisição.
+	 * @param taxaJuros          {@link BigDecimal} : taxa de juros calculada.
+	 */
 	public static BigDecimal calculaTaxaJuros(final BigDecimal valorContrato, final Integer quantidadeParcelas) {
 		final BigDecimal taxaJuros;
 		if (valorContrato.compareTo(VALOR_LIMITE_MENOR_TAXA_JUROS) <= 0) {
@@ -103,6 +146,15 @@ public class ContratoEmprestimoService {
 		return valorParcelas;
 	}
 
+	/**
+	 * Efetua o cálculo para a data de validade do contrato, isso a partir da data
+	 * de simulação informada.<br>
+	 * ► A data de validade será de {@link #DIAS_VALIDADE_SIMULACAO_EMPRESTIMO}
+	 * depois da data de simulação.
+	 * 
+	 * @param dataSimulacao {@link Date} : data de simulação do contrato.
+	 * @return {@link Date} : data de validade calculada.
+	 */
 	public static Date calculaDataValidade(final Date dataSimulacao) {
 		final Calendar calendarValidade = Calendar.getInstance();
 		calendarValidade.setTime(dataSimulacao);
@@ -110,6 +162,15 @@ public class ContratoEmprestimoService {
 		return calendarValidade.getTime();
 	}
 
+	/**
+	 * Verifica se o contrato já está expirado, comparando a data de validade do
+	 * contrato informada com a data atual.<br>
+	 * ► Se a data de validade for menor do que a data atual o contrato é
+	 * considerado expirado.
+	 * 
+	 * @param dataValidadeContrato {@link Date} : data de validade do contrato.
+	 * @return boolean : retorna <b>TRUE</b> caso o contrato esteja expirado.
+	 */
 	public static boolean isContratoExpirado(final Date dataValidadeContrato) {
 		final int comparacaoDataValidadeComAtual = dateOnlyComparator.compare(dataValidadeContrato, new Date());
 		return (comparacaoDataValidadeComAtual < 0);
--- src/main/java/br/com/cresol/desafio/service/PessoaService.java
+++ src/main/java/br/com/cresol/desafio/service/PessoaService.java
@@ -6,6 +6,12 @@ import br.com.cresol.desafio.dto.Pessoa;
 import br.com.cresol.desafio.manager.PessoaManager;
 import br.com.cresol.desafio.util.ValidacaoUtil;
 
+/**
+ * Serviço interno responsável por obter e registrar os dados das pessoas
+ * envolvidas nas requisições de Simulação de Empréstimos.
+ * 
+ * @author Cleiton Janke
+ */
 public class PessoaService {
 
 	private final PessoaManager pessoaManager;
@@ -17,6 +23,21 @@ public class PessoaService {
 		this.pessoaManager = pessoaManager;
 	}
 
+	/**
+	 * Obtém os dados da pessoa envolvida na requisição do empréstimo.<br>
+	 * ► Se alguma pessoa registrada for encontrada pelo parâmetro 'CPF', e os
+	 * demais parâmetros forem os mesmos dos dados deste registros, então apenas
+	 * retorna os dados já registrados.<br>
+	 * ► Se houver alteração nos demais parâmetros, então atualiza os dados e
+	 * retorna o registro.<br>
+	 * ► Se não houver pessoa previamente registrada, cadastra os novos dados.<br>
+	 * 
+	 * @param nome  {@link String} : nome da pessoa.
+	 * @param cpf   {@link String} : CPF da pessoa.
+	 * @param email {@link String} : e-mail da pessoa.
+	 * @return {@link Pessoa} : objeto com os dados registrados da pessoa.
+	 * @throws Exception : exceção ao validar os parâmetros ou obter os dados.
+	 */
 	public Pessoa getDadosPessoa(final String nome, final String cpf, final String email) throws Exception {
 		ValidacaoUtil.validaParametrosParaPessoas(nome, cpf, email);
 
